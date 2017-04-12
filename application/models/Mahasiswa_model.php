57094b5b44a20d3d4e7a2cc5b746b00c2832e307
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-04-12 09:03:55 +0700

    menambahkan method untuk update mahasiswa

--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -2,14 +2,15 @@
 class Mahasiswa_model extends CI_Model {
     public function get_all_mahasiswa(){
         $query = $this->db->get("mahasiswa");
-
-        $nama = "Erick";
-
         return $query->result();
     }
 
     public function insert_mahasiswa($data){
         $this->db->insert('mahasiswa',$data);
+    }
+
+    public function update_mahasiswa($data){
+        $this->db->update('mahasiswa',$data);
     }
 }
 ?>
